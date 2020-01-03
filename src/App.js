248f6e4cfea969fa15e2708582e3ecec5fe0aa1d
Arpan2912 <shaharpan05@gmail.com>
2020-01-03 18:52:29 +0530

    convert functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import { HashRouter as Router, NavLink } from 'react-router-dom';
 
 import { Row, Col } from 'reactstrap';
@@ -8,38 +8,43 @@ import StorageService from './services/StorageService';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const token = StorageService.getToken();
-  return (
-    <div>
-      <Router>
+class App extends Component {
+  
+  render() {
+    const token = StorageService.getToken();
+    return (
+      <div>
+        <Router>
 
-        {/* <header className="App-header">
-      </header> */}
-        <header style={{ height: '50px' }}>
-          <Row>
-            <Col sm="2" style={{ lineHeight: '67px' }}>
-              <span style={{ fontWeight: 'bold', fontSize: '20px', paddingLeft: '20px' }}>Dmd</span>
-            </Col>
-            {token && <Fragment>
-              <Col sm="8" className="text-align-right">
-                <NavLink exact activeClassName="active" className="nav-link" to="/home">Dashboard</NavLink>
-                <NavLink exact activeClassName="active" className="nav-link" to="/person">Person</NavLink>
-                <NavLink exact activeClassName="active" className="nav-link" to="/rough">Rough</NavLink>
+          {/* <header className="App-header">
+        </header> */}
+          <header style={{ height: '50px' }}>
+            <Row>
+              <Col sm="2" style={{ lineHeight: '67px' }}>
+                <span style={{ fontWeight: 'bold', fontSize: '20px', paddingLeft: '20px' }}>Dmd</span>
               </Col>
-              <Col sm="2" className="text-align-right">
-                <NavLink exact activeClassName="active" className="nav-link" to="/logout">Logout</NavLink>
+              {token && <Fragment>
+                <Col sm="8" className="text-align-right">
+                  <NavLink exact activeClassName="active" className="nav-link" to="/home">Dashboard</NavLink>
+                  <NavLink exact activeClassName="active" className="nav-link" to="/person">Person</NavLink>
+                  <NavLink exact activeClassName="active" className="nav-link" to="/rough">Rough</NavLink>
+                </Col>
+                <Col sm="2" className="text-align-right">
+                  <NavLink exact activeClassName="active" className="nav-link" to="/logout">Logout</NavLink>
 
-              </Col>
-            </Fragment>}
-          </Row>
-        </header>
-        <hr />
+                </Col>
+              </Fragment>}
+            </Row>
+          </header>
+          <hr />
 
-        <Routes />
-      </Router>
-    </div>
-  );
+          <Routes />
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
